6f0227d8d9dd0776ea0e138bd9fe46da381df7e2
prernasharma28 <prernasharmacu@gmail.com>
2024-10-08 01:42:08 +0530

    All Done Responsive Project

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,127 +1,3 @@
-// import React, { useRef, useEffect, useContext } from "react";
-// import { Container, Row, Button } from "reactstrap";
-// import { NavLink, Link, useNavigate } from "react-router-dom";
-
-// import logo from "../../assets/images/logo.png";
-// import "./header.css";
-
-// import { AuthContext } from './../../context/AuthContext';
-
-// const navLinks = [
-//   {
-//     path: "/home",
-//     display: "Home",
-//   },
-//   {
-//     path: "/about",
-//     display: "About",
-//   },
-//   {
-//     path: "/contact",
-//     display: "Contact",
-//   },
-//   {
-//     path: "/tours",
-//     display: "Tours",
-//   },
-// ];
-
-// const Header = () => {
-//   const headerRef = useRef(null);
-
-//   const navigate = useNavigate()
-//   const { user, dispatch } = useContext(AuthContext)
-
-//   const logout = () => {
-//     dispatch({ type: 'LOGOUT' })
-//     navigate('/')
-//   }
-
-//   const stickyHeaderFunc = () => {
-//     if (window.scrollY > 80) {
-//       headerRef.current.classList.add("sticky__header");
-//     } else {
-//       headerRef.current.classList.remove("sticky__header");
-//     }
-//   };
-
-//   useEffect(() => {
-//     window.addEventListener("scroll", stickyHeaderFunc);
-
-//     return () => {
-//       window.removeEventListener("scroll", stickyHeaderFunc);
-//     };
-//   }, []);
-
-//   return (
-//     <header className="header" ref={headerRef}>
-//       <Container>
-//         <Row>
-//           <div className="nav__wrapper d-flex align-items-center justify-content-between">
-//             {/* Logo */}
-//             <div className="logo">
-//               <img src={logo} alt="Logo" />
-//             </div>
-//             {/* Logo-end */}
-//             {/* menu start */}
-//             <div className="navigation">
-//               <ul className="menu d-flex align-items-center gap-5">
-//                 {navLinks.map((item, index) => (
-//                   <li className="nav__item" key={index}>
-//                     <NavLink
-//                       to={item.path}
-//                       className={(navClass) =>
-//                         navClass.isActive ? "active__link" : ""
-//                       }
-//                     >
-//                       {item.display}
-//                     </NavLink>
-//                   </li>
-//                 ))}
-//               </ul>
-//             </div>
-//             {/* menu end */}
-//             <div className="nav_right d-flex align-items-center gap-4">
-//               <div className="nav__btns d-flex align-items-center gap-4">
-
-//                 {
-//                   user ? <>
-
-//                     <h5 className="mb-0">{user.username}</h5>
-//                     <Button className="btn  btn-dark" onClick={logout}></Button>
-
-//                   </> :
-//                     <>
-//                       <Button className="btn secondary__btn">
-//                         <Link to="/login">Login</Link>
-//                       </Button>
-//                       <Button className="btn primary__btn">
-//                         <Link to="/register">Register</Link>
-//                       </Button>
-//                     </>
-
-//                 }
-//                 <Button className="btn secondary__btn">
-//                   <Link to="/login">Login</Link>
-//                 </Button>
-//                 <Button className="btn primary__btn">
-//                   <Link to="/register">Register</Link>
-//                 </Button>
-//               </div>
-//               <span className="mobile__menu">
-//                 <i className="ri-menu-line"></i>
-//               </span>
-//             </div>
-//           </div>
-//         </Row>
-//       </Container>
-//     </header>
-//   );
-// };
-
-// export default Header;
-
-
 import React, { useRef, useEffect, useContext } from "react";
 import { Container, Row, Button } from "reactstrap";
 import { NavLink, Link, useNavigate } from "react-router-dom";
@@ -152,6 +28,7 @@ const navLinks = [
 
 const Header = () => {
   const headerRef = useRef(null);
+  const menuRef = useRef(null);
 
   const navigate = useNavigate();
   const { user, dispatch } = useContext(AuthContext);
@@ -177,6 +54,8 @@ const Header = () => {
     };
   }, []);
 
+  const toggleMenu = () => menuRef.current.classList.toggle("show__menu");
+
   return (
     <header className="header" ref={headerRef}>
       <Container>
@@ -188,7 +67,7 @@ const Header = () => {
             </div>
             {/* Logo-end */}
             {/* menu start */}
-            <div className="navigation">
+            <div className="navigation" ref={menuRef} onClick={toggleMenu}>
               <ul className="menu d-flex align-items-center gap-5">
                 {navLinks.map((item, index) => (
                   <li className="nav__item" key={index}>
@@ -225,7 +104,7 @@ const Header = () => {
                   </>
                 )}
               </div>
-              <span className="mobile__menu">
+              <span className="mobile__menu" onClick={toggleMenu}>
                 <i className="ri-menu-line"></i>
               </span>
             </div>
@@ -237,3 +116,5 @@ const Header = () => {
 };
 
 export default Header;
+
+
--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -47,7 +47,7 @@ const Tours = () => {
             !loading && !error && <Row>
             {
               tours?.map
-                (tour => <Col lg='3' key={tour._id}>
+                (tour => <Col lg='3' md='6' sm='6' key={tour._id}>
                 <TourCard tour={tour} />
               </Col>
               )
